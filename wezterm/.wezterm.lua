ed75af172a70b01667cfd82d64da9ca6005bb7d2
Kevin Colyar <kevinc@dcpud.org>
2022-09-09 15:39:36 -0700

    Adds wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm'
+
+return {
+   enable_tab_bar = false,
+   window_decorations = "RESIZE",
+   color_scheme = 'DoomOne',
+   -- color_scheme = 'Dracula (Gogh)',
+   -- color_scheme = "Duotone Dark",
+   -- color_scheme = "Gigavolt (base16)",
+   -- color_scheme = "Later This Evening",
+   font = wezterm.font('Fisa Code'),
+   -- font = wezterm.font('SauceCode Pro Nerd Font Regular'),
+   -- font = wezterm.font('Fisa Code', { weight = 'Thin' }),
+   font_size = 15,
+}
