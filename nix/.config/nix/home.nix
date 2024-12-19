bc63434f868f459e9fdf68ee26afcdc2fa29b59b
Kevin Colyar <kevincolyar@mini.lan>
2024-12-19 13:22:07 -0800

    Misc nix

--- nix/.config/nix/home.nix
+++ nix/.config/nix/home.nix
@@ -35,6 +35,7 @@
     bat
     bottom
     gnupg
+    pinentry-tty
     dua
     fd
     htop
@@ -49,6 +50,7 @@
     yazi
     nmap
     prettierd # html formatter, used by emacs format-all 
+    iperf
 
     zsh
     zsh-syntax-highlighting
@@ -132,6 +134,7 @@
   #
   home.sessionVariables = {
     EDITOR = "emacs";
+    GPG_TTY = "tty"; # Required by gnupg-vim
   };
 
   programs.starship.enable = true;
