f1f3c8eb8483e76c4e7eae9bf605377149c2cf8d
Kevin Colyar <kevin@colyar.net>
2026-08-20 16:28:28 -0700

    Home manager backup file extension

--- nix/.config/nix/mini.nix
+++ nix/.config/nix/mini.nix
@@ -45,6 +45,8 @@
       };
     };
   };
+  
+  home-manager.backupFileExtension = "backup";
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
